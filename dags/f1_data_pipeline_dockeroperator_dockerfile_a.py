563a2dd87b166b647f76dcb6096a97b3cab7352b
hafizhtrinelzy <hafizhtrinelzy2002@gmail.com>
2026-05-11 22:45:02 +0700

    .

--- dags/f1_data_pipeline_dockeroperator_dockerfile_a.py
+++ dags/f1_data_pipeline_dockeroperator_dockerfile_a.py
@@ -179,7 +179,7 @@ session_dates = sorted(
         "execution_timeout": timedelta(hours=1),
         "email_on_retry": True,
     },
-    start_date=datetime(2024, 12, 7),
+    start_date=datetime(2026, 5, 10),
     schedule="@daily",
     catchup=True,
     max_active_runs=1,
